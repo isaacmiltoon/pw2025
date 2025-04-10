711c5751a8bf6d8b063f79f55b529743671fa3c1
Professor <rafael.zottesso@ifpr.edu.br>
2025-04-10 10:39:21 -0300

    Reutilização de templates

--- paginasweb/urls.py
+++ paginasweb/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import IndexView, SobreView
 
 urlpatterns = [
+    
     path('', IndexView.as_view(), name='index'),  # URL para a página inicial
-    path('sobre/', SobreView.as_view(), name='sobre'),  # URL para a página "Sobre"
+    path("sobre/", SobreView.as_view(), name="sobre"),
+
 ]
