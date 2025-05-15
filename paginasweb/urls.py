a75ec8d57cda9140e938cc379016f7f0b297b5a3
Professor <rafael.zottesso@ifpr.edu.br>
2025-05-15 08:32:55 -0300

    Views para inserir objetos no banco de dados; Formulário HTML padrão

--- paginasweb/urls.py
+++ paginasweb/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from .views import IndexView, SobreView
+from .views import CampusCreate, CursoCreate, TipoSolicitacaoCreate, StatusCreate, AlunoCreate, ServidorCreate, SolicitacaoCreate, HistoricoCreate
 
 urlpatterns = [
     
     path('', IndexView.as_view(), name='index'),  # URL para a página inicial
     path("sobre/", SobreView.as_view(), name="sobre"),
+
+    path("cadastrar/campus/", CampusCreate.as_view(), name="cadastrar-campus"),  # URL para criar um novo campus
+    path("cadastrar/curso/", CursoCreate.as_view(), name="cadastrar-curso"),  # URL para criar um novo curso
+    path("cadastrar/tipo-solicitacao/", TipoSolicitacaoCreate.as_view(), name="cadastrar-tipo-solicitacao"),  # URL para criar um novo tipo de solicitação
+    path("cadastrar/status/", StatusCreate.as_view(), name="cadastrar-status"),  # URL para criar um novo status
+    path("cadastrar/aluno/", AlunoCreate.as_view(), name="cadastrar-aluno"),  # URL para criar um novo aluno
+    path("cadastrar/servidor/", ServidorCreate.as_view(), name="cadastrar-servidor"),  # URL para criar um novo servidor
+    path("cadastrar/solicitacao/", SolicitacaoCreate.as_view(), name="cadastrar-solicitacao"),  # URL para criar uma nova solicitação
+    path("cadastrar/historico/", HistoricoCreate.as_view(), name="cadastrar-historico"),  # URL para criar um novo histórico
+
+    
 
 ]
--- paginasweb/views.py
+++ paginasweb/views.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
+from .models import Campus, Curso, TipoSolicitacao, Status, Aluno, Servidor, Solicitacao, Historico
 
 
 class IndexView(TemplateView):
@@ -7,3 +11,91 @@ class IndexView(TemplateView):
 
 class SobreView(TemplateView):
     template_name = 'paginasweb/sobre.html'
+
+
+class CampusCreate(CreateView):
+    model = Campus
+    template_name = 'paginasweb/form.html'
+    fields = ['nome']
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastrar Campus',
+        'botao': 'Cadastrar',
+    }
+
+
+class CursoCreate(CreateView):
+    model = Curso
+    template_name = 'paginasweb/form.html'
+    fields = ['nome', 'campus']
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastrar Curso',
+        'botao': 'Cadastrar',
+    }
+
+
+class TipoSolicitacaoCreate(CreateView):
+    model = TipoSolicitacao
+    template_name = 'paginasweb/form.html'
+    fields = ['descricao', 'prazo_externo', 'prazo_externo_dias', 'prazo_interno', 'prazo_interno_dias']
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastrar Tipo de Solicitação',
+        'botao': 'Cadastrar',
+    }
+
+
+class StatusCreate(CreateView):
+    model = Status
+    template_name = 'paginasweb/form.html'
+    fields = ['nome', 'ordem', 'pode_editar']
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastrar Status',
+        'botao': 'Cadastrar',
+    }
+
+
+class AlunoCreate(CreateView):
+    model = Aluno
+    template_name = 'paginasweb/form.html'
+    fields = ['nome', 'matrícula', 'cpf', 'email', 'telefone']
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastrar Aluno',
+        'botao': 'Cadastrar',
+    }
+
+
+class ServidorCreate(CreateView):
+    model = Servidor
+    template_name = 'paginasweb/form.html'
+    fields = ['nome', 'siape', 'email']
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastrar Servidor',
+        'botao': 'Cadastrar',
+    }
+
+
+class SolicitacaoCreate(CreateView):
+    model = Solicitacao
+    template_name = 'paginasweb/form.html'
+    fields = ['solicitado_por', 'curso', 'turma', 'tipo_solicitação', 'justificativa']
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastrar Solicitação',
+        'botao': 'Cadastrar',
+    }
+
+
+class HistoricoCreate(CreateView):
+    model = Historico
+    template_name = 'paginasweb/form.html'
+    fields = ['solicitacao', 'status']
+    success_url = reverse_lazy('index')
+    extra_context = {
+        'titulo': 'Cadastrar Histórico',
+        'botao': 'Cadastrar',
+    }
